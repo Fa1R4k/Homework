fe485885fb21bfe8182bf4aeba6417a025e3bb27
Max Zavalo <max.mine2104@gmail.com>
2022-11-16 18:44:43 +0300

    homework task1

--- src/Homework.java
+++ src/Homework.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Homework {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your number: ");
+        if (scanner.nextInt() % 2 == 0) {
+            System.out.println("Число четное!");
+        } else {
+            System.out.println("Число нечетное");
+        }
     }
 }
