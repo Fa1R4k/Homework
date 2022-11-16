05b51e8e20e7bb100c4d9f668c5e806acd1fe9ec
Max Zavalo <max.mine2104@gmail.com>
2022-11-16 18:53:18 +0300

    homework task4

--- src/Homework.java
+++ src/Homework.java
@@ -65,8 +65,8 @@ public class Homework {
     }
 
     public static void task4() {
-        for (int i = 0; i <= 100; i+=5) {
-            if(i != 0){
+        for (int i = 0; i <= 100; i += 5) {
+            if (i != 0) {
                 System.out.println(i);
             }
         }
